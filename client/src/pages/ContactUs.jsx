82e1484fe1e28eeeb67e0b4ed780826a572b9ddd
luka1sys <dgebuadzeluka2008@gmail.com>
2026-03-16 00:21:30 +0400

    commit

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -120,7 +120,7 @@ const ContactUs = () => {
                     <iframe
                         title="map"
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2977.614330616182!2d44.7745!3d41.7289!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDHCsDQzJzQ0LjAiTiA0NMKwNDYnMjguMiJF!5e0!3m2!1sen!2sge!4v1700000000000"
-                        className="w-full h-full border-0 grayscale hover:grayscale-0 transition-all duration-1000"
+                        className="w-full h-full border-0  transition-all duration-1000"
                         allowFullScreen=""
                         loading="lazy"
                     ></iframe>
